b6e6cc511f7e1897b50e30670f848b84d5fc6c9a
Mauricio Araujo <mauricioas@weg.net>
2023-04-10 15:51:44 -0300

    ngDoCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-title></app-title>
+    {{ valor }}
+    <button (click)="add()">Add</button>
     <router-outlet></router-outlet>
   `,
 })
-export class AppComponent implements OnInit {
+export class AppComponent
+  implements
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
+  public valor: number = 1;
+
   constructor() {}
 
+  public add(): number {
+    return (this.valor += 1);
+  }
+
   ngOnInit(): void {}
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+    // throw new Error('Method not implemented.');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+    // throw new Error('Method not implemented.');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+    // throw new Error('Method not implemented.');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+    // throw new Error('Method not implemented.');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+    // throw new Error('Method not implemented.');
+  }
 }
