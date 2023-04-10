2459ef00a3764fd5cb304d0c36517b48078adc0f
Mauricio Araujo <mauricioas@weg.net>
2023-04-10 16:01:56 -0300

    ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,48 +11,20 @@ import {
 @Component({
   selector: 'app-root',
   template: `
-    {{ valor }}
-    <button (click)="add()">Add</button>
+    <app-title *ngIf="destruir"></app-title>
+    <br />
+    <button (click)="destruirComponent()">Destruir componente</button>
     <router-outlet></router-outlet>
   `,
 })
-export class AppComponent
-  implements
-    OnInit,
-    DoCheck,
-    AfterContentInit,
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked
-{
-  public valor: number = 1;
+export class AppComponent implements OnInit {
+  public destruir: boolean = true;
 
   constructor() {}
 
-  public add(): number {
-    return (this.valor += 1);
-  }
-
   ngOnInit(): void {}
 
-  ngDoCheck(): void {
-    console.log('ngDoCheck');
-    // throw new Error('Method not implemented.');
-  }
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-    // throw new Error('Method not implemented.');
-  }
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
-    // throw new Error('Method not implemented.');
-  }
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-    // throw new Error('Method not implemented.');
-  }
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked');
-    // throw new Error('Method not implemented.');
+  public destruirComponent() {
+    this.destruir = false;
   }
 }
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent implements OnInit, OnChanges {
-  @Input() public title: string = 'Welcome';
+export class TitleComponent implements OnInit, OnChanges, OnDestroy {
+  public title: string = 'Welcome';
 
   constructor() {}
 
   ngOnInit(): void {}
 
   ngOnChanges(): void {}
+
+  ngOnDestroy(): void {
+    console.log('Destroyed');
+  }
 }
