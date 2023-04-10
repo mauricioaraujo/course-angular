5f846d8ccfe4c7e1073e873d3c6de848748cb95a
Mauricio Araujo <mauricioas@weg.net>
2023-04-10 15:28:27 -0300

    ngOnChanges

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent {
-  public title: string = 'Welcome';
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() public title: string = 'Welcome';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {}
 }
